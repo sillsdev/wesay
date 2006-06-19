71f39f70e42d70c8921e0e92fe68a2997d774bc8
John Hatton <hattonjohn@gmail.com>
2006-06-19 06:30:13 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@41 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/Core/LexicalEntry.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeSay.Core
+{
+	public class LexicalEntry
+	{
+		private string _lexicalForm;
+		private string _gloss;
+		private string _example;
+
+		public LexicalEntry()
+		{
+
+		}
+
+		public void LoadFromTestXml(System.Xml.XmlNode node)
+		{
+			_lexicalForm = GetFieldFromTestXml(node, "lexeme","unknown!");
+			_gloss = GetFieldFromTestXml(node,"gloss",null);
+			_example = GetFieldFromTestXml(node, "example",null);
+	  }
+		private static string GetFieldFromTestXml(System.Xml.XmlNode node, string elementName, string defaultValue)
+		{
+			System.Xml.XmlNode n= node.SelectSingleNode(elementName);
+			if(n==null)
+				return defaultValue;
+			return n.InnerText;
+		}
+
+
+	}
+}
