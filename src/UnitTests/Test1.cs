9f1397001253b5849918246ecc406ae7ecea4417
John Hatton <hattonjohn@gmail.com>
2006-06-19 05:43:25 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@38 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/UnitTests/Test1.cs
+++ src/UnitTests/Test1.cs
@@ -1,14 +1,114 @@
 using NUnit.Framework;
+using com.db4o;
+using System.Diagnostics;
+using WeSay.Core;
+using System.Xml;
 
 namespace WeSay.UnitTests
 {
+	using System.Diagnostics;
+	public class ProcessMemory
+	{
+		public static long WorkingSet
+		{
+			get { return Process.GetCurrentProcess().WorkingSet64; }
+		}
+		public static void Write(string s)
+		{
+			Debug.WriteLine( ProcessMemory.WorkingSet+ " "+s);
+		}
+	}
+
 	[TestFixture]
 	public class Test1
 	{
 		[Test]
-		public void ThisIsAnExampleTest()
+		public void Load()
 		{
-			Assert.AreEqual(2, 1+1);
+			JetBrains.dotTrace.Api.CPUProfiler.Start();
+
+			ObjectContainer database = GetBlankDb("thai5000.yap");
+			try
+			{
+				XmlDocument document = new XmlDocument();
+				document.Load(@"c:\WeSay\src\unittests\thai5000.xml");
+			   foreach(XmlNode node in document.SelectNodes("test/entry"))
+			   {
+				   LexicalEntry entry = new LexicalEntry();
+				   entry.LoadFromTestXml(node);
+
+				   database.Set(entry);
+			   }
+			}
+			finally
+			{
+				database.Close();
+			}
+			JetBrains.dotTrace.Api.CPUProfiler.StopAndSaveSnapShot();
+		}
+
+		[Test]
+		public void Read()
+		{
+			ObjectContainer database = GetDb("thai5000.yap");
+			try
+			{
+				ProcessMemory.Write("Start of READ");
+
+				com.db4o.query.Query query = database.Query();
+				//query.Constrain(typeof(UtteranceHistory));
+				//query.Descend("_utteranceKey").Constrain(this.GUID.ToString());
+				com.db4o.config.Configuration config = Db4o.Configure();
+				//config.ActivationDepth(0);
+
+				ProcessMemory.Write("After open");
+
+				ObjectSet result = query.Execute();
+				foreach (LexicalEntry entry in result)
+				{
+				}
+				ProcessMemory.Write("After Touching all");
+		   }
+			finally
+			{
+				database.Close();
+			}
+		   ProcessMemory.Write("After db closed");
+		}
+
+		private ObjectContainer GetDb(string name)
+		{
+			ObjectContainer db;
+			string s = @"c:\WeSay\src\unittests\"+name;
+			db = Db4oFactory.OpenFile(s);
+
+			Assert.IsNotNull(db);
+			com.db4o.config.Configuration config = Db4o.Configure();
+			//            SetupIndices(config);
+
+			return db;
+		}
+
+		private ObjectContainer GetBlankDb(string name)
+		{
+			ObjectContainer db;
+			string s = @"c:\WeSay\src\unittests\"+name;
+			System.IO.File.Delete(s);
+			db = Db4oFactory.OpenFile(s);
+
+			Assert.IsNotNull(db);
+			com.db4o.config.Configuration config = Db4o.Configure();
+//            SetupIndices(config);
+
+			return db;
+		}
+
+		private static void SetupIndices(com.db4o.config.Configuration config)
+		{
+			LexicalEntry dummy = new LexicalEntry();
+			com.db4o.config.ObjectClass oc = config.ObjectClass(dummy);
+			com.db4o.config.ObjectField of = oc.ObjectField("_lexicalForm");
+			of.Indexed(true);
 		}
 	}
 }
