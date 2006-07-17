461901ba58cbdb0c7eed1cf2e2cdec2ae8ad9dcf
John Hatton <hattonjohn@gmail.com>
2006-07-17 03:02:13 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@97 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/LexicalModel/LiftExporter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace WeSay.LexicalModel
+{
+	public class LiftExporter
+	{
+	   public static void Write(XmlWriter writer, LexicalEntry entry)
+		{
+			writer.WriteStartElement("entry");
+
+			WriteMultilingualString(writer, "lexicalForm", entry.LexicalForm);
+			WriteMultilingualString(writer, "gloss", entry.Gloss);
+			WriteMultilingualString(writer, "example", entry.Example);
+			writer.WriteEndElement();
+		 }
+
+		private static void WriteMultilingualString(XmlWriter writer, string elementName, string s)
+		{
+			writer.WriteStartElement(elementName);
+			writer.WriteAttributeString("ws", "??");
+			writer.WriteString(s);
+			writer.WriteEndElement();
+		}
+	}
+}
