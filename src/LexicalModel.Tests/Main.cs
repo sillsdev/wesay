3133a82b93fba15a16da704352185d08d812c807
John Hatton <hattonjohn@gmail.com>
2006-08-04 08:55:08 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@124 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/LexicalModel.Tests/Main.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Xml;
+using System.Collections.Generic;
+using WeSay.LexicalModel;
+
+namespace LexicalModel.Tests
+{
+	[TestFixture]
+	public class Test1
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Dummy()
+		{
+
+		}
+	}
+}
---
+++ src/LexicalModel/LexicalEntry.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeSay.LexicalModel
+{
+	public class LexicalEntry
+	{
+		public LexicalEntry()
+		{
+		}
+
+	}
+}
