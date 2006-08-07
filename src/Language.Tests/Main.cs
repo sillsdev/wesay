0a08d637599ef40f4517c28c8c33a8edf7aec123
John Hatton <hattonjohn@gmail.com>
2006-08-07 02:22:43 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@129 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/Language.Tests/Main.cs
+++ src/Language.Tests/Main.cs
@@ -14,9 +14,35 @@ namespace Language.Tests
 		}
 
 		[Test]
-		public void Dummy()
+		public void NullConditions()
 		{
+			MultiText text = new MultiText();
+			Assert.AreSame("", text["foo"], "never before heard of alternative should give back an empty string");
+			Assert.AreSame("", text["foo"], "second time");
+			Assert.AreSame("", text.GetAlternative("fox"));
+			text.SetAlternative("zox", "");
+			Assert.AreSame("", text["zox"]);
+			text.SetAlternative("zox", null);
+			Assert.AreSame("", text["zox"], "should still be empty string after setting to null");
+			text.SetAlternative("zox", "something");
+			text.SetAlternative("zox", null);
+			Assert.AreSame("", text["zox"], "should still be empty string after setting something and then back to null");
+		}
 
+		[Test]
+		public void BasicStuff()
+		{
+			MultiText text = new MultiText();
+			text["foo"] = "alpha";
+			Assert.AreSame("alpha", text["foo"]);
+			text["foo"] = "beta";
+			Assert.AreSame("beta", text["foo"]);
+			text["foo"] = "gamma";
+			Assert.AreSame("gamma", text["foo"]);
+			text["bee"] = "beeeee";
+			Assert.AreSame("gamma", text["foo"], "setting a different alternative should not affect this one");
+			text["foo"] = null;
+			Assert.AreSame("", text["foo"]);
 		}
 	}
 }
--- src/Language/MultiText.cs
+++ src/Language/MultiText.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace WeSay.Language
 {
+	/// <summary>
+	/// MultiText holds an array of strings, indexed by writing system ID.
+	/// These are simple, single language Unicode strings.
+	/// </summary>
 	public class MultiText
 	{
 		protected System.Collections.Generic.Dictionary<string, string> _forms;
@@ -12,15 +17,47 @@ namespace WeSay.Language
 			_forms = new Dictionary<string, string>();
 		}
 
+		public string this[string writingSystemId]
+		{
+			get
+			{
+				return GetAlternative(writingSystemId);
+			}
+			set
+			{
+				SetAlternative(writingSystemId, value);
+			}
+		}
+
 		public string GetAlternative(string writingSystem)
 		{
+			if (!_forms.ContainsKey(writingSystem))
+				return "";
+
 			string form = _forms[writingSystem];
 			if (form == null)
 			{
-				return null;
+				return "";
 			}
 			else
 				return form;
 		}
+
+		public void SetAlternative(string writingSystem, string form)
+		{
+		   Debug.Assert(writingSystem != null && writingSystem.Length > 0, "The writing system id was empty.");
+		   Debug.Assert(writingSystem.Trim() == writingSystem, "The writing system id had leading or trailing whitespace");
+
+			if (form == null || form == "" ) // we don't use space to store empty strings.
+			{
+				if (_forms.ContainsKey(writingSystem))
+				{
+					_forms.Remove(writingSystem);
+				}
+			}
+
+			_forms[writingSystem] = form;
+		}
+
 	}
 }
