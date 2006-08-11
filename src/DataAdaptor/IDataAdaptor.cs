ab4294396fb2439162f1b48344cffb23bdb84872
Eric Albright <eric_albright@sil.org>
2006-08-11 02:02:05 +0000

    Db40 BindingList
    git-svn-id: http://www.wesay.org/code/WeSay/trunk@145 b25276a9-ce15-0410-9281-991c76e3d784
    
    committer: albright <albright@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/DataAdaptor/IDataAdaptor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeSay.DataAdaptor
+{
+	public interface IDataAdaptor
+	{
+	}
+
+	public interface IDataSource
+	{
+	}
+}
