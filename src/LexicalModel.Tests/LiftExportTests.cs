cd624c925edc3a1466106cdc3a977c2b25d1c3cf
John Hatton <hattonjohn@gmail.com>
2006-09-07 09:06:55 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@239 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/LexicalModel.Tests/LiftExportTests.cs
+++ src/LexicalModel.Tests/LiftExportTests.cs
@@ -1,6 +1,9 @@
 using System.IO;
+using System.Text;
+using System.Xml;
 using NUnit.Framework;
 using WeSay.Data;
+using WeSay.Language;
 using WeSay.LexicalModel;
 
 namespace WeSay.LexicalTools.Tests
@@ -9,21 +12,143 @@ namespace WeSay.LexicalTools.Tests
 	public class LiftExportTests
 	{
 		private Db4oBindingList<LexEntry> _entriesList;
-		private string _filePath ;
-
+		private LiftExporter _exporter;
+		private StringBuilder _stringBuilder;
 
 		[SetUp]
 		public void Setup()
 		{
-			_filePath = Path.GetTempFileName();
+			_stringBuilder = new StringBuilder();
+			XmlWriterSettings settings = new XmlWriterSettings();
+			_exporter = new LiftExporter(_stringBuilder, true);
 		}
+
 		[TearDown]
 		public void TearDown()
 		{
-			this._entriesList.Dispose();
-			File.Delete(_filePath);
+//            this._entriesList.Dispose();
+		}
+
+		[Test]
+		public void MultiText()
+		{
+			MultiText text = new MultiText();
+			text["blue"] = "ocean";
+			text ["red"] = "sunset";
+			_exporter.Add(text);
+			CheckAnswer("<form lang=\"blue\">ocean</form><form lang=\"red\">sunset</form>");
 		}
 
 
+		[Test]
+		public void Sense()
+		{
+			LexSense sense = new LexSense();
+			sense.Gloss["blue"] = "ocean";
+			_exporter.Add(sense);
+			CheckAnswer("<sense><gloss><form lang=\"blue\">ocean</form></gloss></sense>");
+		}
+
+
+		[Test]
+		public void BlankMultiText()
+		{
+			_exporter.Add(new MultiText());
+			CheckAnswer("");
+		}
+
+		[Test]
+		public void BlankExample()
+		{
+			_exporter.Add(new LexExampleSentence());
+			CheckAnswer("<example />");
+		}
+
+		[Test]
+		public void SmallExample()
+		{
+			LexExampleSentence example = new LexExampleSentence();
+			example.Sentence["blue"] = "ocean's eleven";
+			example.Sentence["red"] = "red sunset tonight";
+			_exporter.Add(example);
+			CheckAnswer("<example><source><form lang=\"blue\">ocean's eleven</form><form lang=\"red\">red sunset tonight</form></source></example>");
+		}
+
+		[Test]
+		public void FullExample()
+		{
+			LexExampleSentence example = new LexExampleSentence();
+			example.Sentence["blue"] = "ocean's eleven";
+			example.Sentence["red"] = "red sunset tonight";
+			example.Translation["green"] = "blah blah";
+			_exporter.Add(example);
+			CheckAnswer("<example><source><form lang=\"blue\">ocean's eleven</form><form lang=\"red\">red sunset tonight</form></source><trans><form lang=\"green\">blah blah</form></trans></example>");
+		}
+
+		[Test]
+		public void BlankSense()
+		{
+			_exporter.Add(new LexSense());
+			CheckAnswer("<sense />");
+		}
+
+		[Test]
+		public void BlankEntry()
+		{
+			_exporter.Add(new LexEntry());
+			CheckAnswer("<entry />");
+		}
+
+		[Test]
+		public void SmallEntry()
+		{
+			 LexEntry entry = new LexEntry();
+			 entry.LexicalForm["blue"] = "ocean";
+			 _exporter.Add(entry);
+			CheckAnswer("<entry><form lang=\"blue\">ocean</form></entry>");
+		}
+
+		[Test]
+		public void EntryWithSenses()
+		{
+			LexEntry entry = new LexEntry();
+			entry.LexicalForm["blue"] = "ocean";
+			LexSense sense = new LexSense();
+			sense.Gloss["a"] = "aaa";
+			entry.Senses.Add(sense);
+			sense = new LexSense();
+			sense.Gloss["b"] = "bbb";
+			entry.Senses.Add(sense);
+			_exporter.Add(entry);
+			 CheckAnswer("<entry><form lang=\"blue\">ocean</form></entry><sense><gloss><form lang=\"a\">aaa</form></gloss></sense><sense><gloss><form lang=\"b\">bbb</form></gloss></sense>");
+		}
+
+
+		[Test]
+		public void SenseWithExample()
+		{
+			LexSense sense = new LexSense();
+			LexExampleSentence example = new LexExampleSentence();
+			example.Sentence["red"] = "red sunset tonight";
+			sense.ExampleSentences.Add(example);
+			_exporter.Add(sense);
+			CheckAnswer("<sense><example><source><form lang=\"red\">red sunset tonight</form></source></example></sense>");
+		}
+
+		[Test]
+		public void DocStructure()
+		{
+			XmlWriterSettings settings = new XmlWriterSettings();
+			settings.ConformanceLevel = ConformanceLevel.Document;
+			_exporter = new LiftExporter(_stringBuilder, false);
+			CheckAnswer("<?xml version=\"1.0\" encoding=\"utf-16\"?><lift />");
+		}
+
+		private void CheckAnswer(string answer)
+		{
+			_exporter.End();
+			Assert.AreEqual(answer,
+							_stringBuilder.ToString());
+		}
 	}
 }
--- src/LexicalModel/LiftExporter.cs
+++ src/LexicalModel/LiftExporter.cs
@@ -2,27 +2,121 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
+using WeSay.Language;
 
 namespace WeSay.LexicalModel
 {
 	public class LiftExporter
 	{
-//       public static void Write(XmlWriter writer, LexicalEntry entry)
-//        {
-//            writer.WriteStartElement("entry");
-//
-//            WriteMultilingualString(writer, "lexicalForm", entry.LexicalForm);
-//            WriteMultilingualString(writer, "gloss", entry.Gloss);
-//            WriteMultilingualString(writer, "example", entry.Example);
-//            writer.WriteEndElement();
-//         }
-//
-//        private static void WriteMultilingualString(XmlWriter writer, string elementName, string s)
-//        {
-//            writer.WriteStartElement(elementName);
-//            writer.WriteAttributeString("ws", "??");
-//            writer.WriteString(s);
-//            writer.WriteEndElement();
-//        }
+		protected XmlWriter _writer;
+
+		public LiftExporter(string path)
+		{
+			_writer = XmlWriter.Create(path, PrepareSettings(false));
+			Start();
+		}
+
+		/// <summary>
+		/// for automated testing
+		/// </summary>
+		/// <param name="builder"></param>
+		public LiftExporter(StringBuilder builder, bool produceFragmentOnly)
+		{
+
+			_writer = XmlWriter.Create(builder, PrepareSettings(produceFragmentOnly));
+			if (!produceFragmentOnly)
+			{
+				Start();
+			}
+		}
+
+		private XmlWriterSettings PrepareSettings(bool produceFragmentOnly)
+		{
+			XmlWriterSettings settings = new XmlWriterSettings();
+			if (produceFragmentOnly)
+				settings.ConformanceLevel = ConformanceLevel.Fragment;
+
+			settings.Encoding = Encoding.UTF8;
+			settings.CloseOutput = true;
+			return settings;
+		}
+
+		private void Start()
+		{
+			_writer.WriteStartDocument();
+			_writer.WriteStartElement("lift");
+		}
+
+		public void End()
+		{
+			if (_writer.Settings.ConformanceLevel != ConformanceLevel.Fragment)
+			{
+				_writer.WriteEndElement();//lift
+				_writer.WriteEndDocument();
+			}
+			_writer.Close();
+		}
+
+		public void Add(LexEntry entry)
+		{
+			_writer.WriteStartElement("entry");
+			WriteForm(entry.LexicalForm);
+
+			foreach(LexSense sense in entry.Senses)
+			{
+				Add(sense);
+			}
+		}
+
+		public void Add(LexExampleSentence example)
+		{
+			_writer.WriteStartElement("example");
+			WriteFormInElement("source", example.Sentence);
+			WriteFormInElement("trans", example.Translation);
+			_writer.WriteEndElement();
+		}
+
+		public void Add(MultiText text)
+		{
+			foreach (LanguageForm form in text)
+			{
+				_writer.WriteStartElement("form");
+				_writer.WriteAttributeString("lang", form.WritingSystemId);
+				_writer.WriteString(form.Form);
+				_writer.WriteEndElement();
+			}
+		}
+
+		public void Add(LexSense sense)
+		{
+			_writer.WriteStartElement("sense");
+			WriteFormInElement("gloss", sense.Gloss);
+			foreach (LexExampleSentence example in sense.ExampleSentences)
+			{
+				Add(example);
+			}
+			_writer.WriteEndElement();
+		}
+
+		private void WriteForm(MultiText text)
+		{
+			if (text != null && text.Count > 0)
+			{
+				Add(text);
+			}
+			_writer.WriteEndElement();
+		}
+
+		private void WriteFormInElement(string name, MultiText text)
+		{
+			if (text != null && text.Count > 0)
+			{
+				_writer.WriteStartElement(name);
+				Add(text);
+				_writer.WriteEndElement();
+			}
+		}
+
+
 	}
 }
