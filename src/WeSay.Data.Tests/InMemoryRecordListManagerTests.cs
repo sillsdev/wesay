dc71fc0dc8f68428584747e909a8ec28b651f2f6
Eric Albright <eric_albright@sil.org>
2006-09-25 05:23:51 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@283 b25276a9-ce15-0410-9281-991c76e3d784
    committer: albright <albright@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/WeSay.Data.Tests/InMemoryRecordListManagerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace WeSay.Data.Tests
+{
+	[TestFixture]
+	class InMemoryRecordListManagerTests : RecordListManagerBaseTests
+	{
+		[SetUp]
+		public override void Setup()
+		{
+			_recordListManager = new Db4oRecordListManager();
+			base.Setup();
+		}
+	}
+}
