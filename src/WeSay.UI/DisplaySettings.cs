b14d17f9ae7c7f43a6551985a764ef9cc77cfd18
Eric Albright <eric_albright@sil.org>
2006-10-11 01:12:02 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@339 b25276a9-ce15-0410-9281-991c76e3d784
    committer: albright <albright@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/WeSay.UI/DisplaySettings.cs
@@ -0,0 +1,29 @@
+using System.Drawing;
+
+namespace WeSay.UI
+{
+	public class DisplaySettings
+	{
+		public static DisplaySettings Default = new DisplaySettings(Color.FromArgb(248,255,255));
+
+		public DisplaySettings()
+		{
+
+		}
+		public DisplaySettings(Color backgroundColor)
+		{
+			_backgroundColor = backgroundColor;
+		}
+
+		private Color _backgroundColor;
+		public Color BackgroundColor
+		{
+			get
+			{
+				return _backgroundColor;
+			}
+		}
+
+
+	}
+}
