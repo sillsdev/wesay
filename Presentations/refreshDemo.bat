ffae6d204872d2edf8cec69b2d0ac1379c276bac
John Hatton <hattonjohn@gmail.com>
2006-10-31 16:43:01 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@411 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- Presentations/refreshDemo.bat
+++ Presentations/refreshDemo.bat
@@ -2,3 +2,4 @@ REM 	mkdir c:\ctcdemo
 xcopy thai0\*.* c:\ctcdemo\thai0 /E /Y
 xcopy thai1\*.* c:\ctcdemo\thai1 /E /Y
 xcopy thai2\*.* c:\ctcdemo\thai2 /E /Y
+del "C:\ctcdemo\thai1\WeSay\incremental xml backup\*.*" /Q
