f46168145d17257c323dd6ffd5587a1176840d37
John Hatton <hattonjohn@gmail.com>
2007-02-22 08:06:07 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@518 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ bld/MakeBiatah.bat
@@ -0,0 +1,3 @@
+xcopy /I /Y /E ..\SampleProjects\src\Biatah ..\SampleProjects\Biatah
+..\output\debug\Lift2Wesay "C:\WeSay\SampleProjects\Biatah\wesay\biatah.lift.xml" "C:\WeSay\SampleProjects\Biatah\wesay\biatah.words"
+pause
