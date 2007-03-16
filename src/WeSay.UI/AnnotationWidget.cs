698c7663934c7fe639af922170518315131db764
John Hatton <hattonjohn@gmail.com>
2007-03-16 02:45:47 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@563 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/WeSay.UI/AnnotationWidget.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WeSay.Language;
+
+namespace WeSay.UI
+{
+	public class AnnotationWidget
+	{
+		private FlagButton _flagButton;
+
+		/// <summary>
+		/// This will be referencing the actual annotation of the object
+		/// </summary>
+		private Annotation _annotation;
+
+		private MultiText _multitext;
+		private string _writingSystemId;
+
+		public AnnotationWidget(MultiText multitext, string writingSystemId)
+		{
+			_multitext = multitext;
+			_writingSystemId = writingSystemId;
+		}
+
+		public bool FlagIsOn
+		{
+			get
+			{
+				if (_flagButton != null)
+				{
+					return _multitext.GetAnnotationOfAlternativeIsStarred(_writingSystemId);
+				}
+				return false;
+			}
+
+			set
+			{
+				if (_flagButton != null)
+				{
+					_flagButton.IsSetOn = value;
+
+					_multitext.SetAnnotationOfAlternativeIsStarred(_writingSystemId, value);
+				}
+			}
+		}
+
+		public Control MakeControl(Size panelSize)
+		{
+			_flagButton = new FlagButton();
+			_flagButton.Size = new Size(20, 20);
+			_flagButton.Location = new Point(
+				-1 + panelSize.Width - _flagButton.Width,
+				-1 + panelSize.Height - _flagButton.Height);
+			_flagButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+			_flagButton.Click += new EventHandler(OnClickFlagButton);
+			_flagButton.TabStop = false;
+			_flagButton.IsSetOn = this.FlagIsOn;
+
+
+			//            Panel panel = new Panel();
+			//            panel.Size = flagButton.Size;
+			//            panel.Location = flagButton.Location;
+			//            panel.Anchor = flagButton.Anchor;
+			//            panel.BackColor = System.Drawing.Color.Red;
+
+			return _flagButton;
+		}
+
+		private void OnClickFlagButton(object sender, EventArgs e)
+		{
+			FlagButton b = (FlagButton)sender;
+			this.FlagIsOn = !b.IsSetOn;
+		}
+	}
+}
