eab02be8f6d0cb2502cf0c6b7a948ba9829fc9ef
John Hatton <hattonjohn@gmail.com>
2007-05-16 09:47:57 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@750 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

---
+++ src/WeSay.Foundation/CommonEnumerations.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace WeSay.Foundation
+{
+	public class CommonEnumerations
+	{
+		public enum VisibilitySetting {Invisible, Visible, ReadOnly};
+	}
+}
