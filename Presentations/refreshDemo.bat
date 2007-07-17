52950f4a809c8e314ee3d41bb0884fdae68e9624
Eric Albright <eric_albright@sil.org>
2007-07-17 03:01:11 +0000

    Moved remotely
    git-svn-id: http://www.wesay.org/code/WeSay/trunk@825 b25276a9-ce15-0410-9281-991c76e3d784
    
    committer: albright <albright@b25276a9-ce15-0410-9281-991c76e3d784>

--- Presentations/refreshDemo.bat
+++
@@ -1,5 +0,0 @@
-REM 	mkdir c:\ctcdemo
-xcopy thai0\*.* c:\ctcdemo\thai0 /E /Y
-xcopy thai1\*.* c:\ctcdemo\thai1 /E /Y
-xcopy thai2\*.* c:\ctcdemo\thai2 /E /Y
-del "C:\ctcdemo\thai1\WeSay\incremental xml backup\*.*" /Q
