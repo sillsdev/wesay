854bd9383c7bec7e415b89e14e08df7e6dd10300
Eric Albright <eric_albright@sil.org>
2007-09-24 05:03:54 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@925 b25276a9-ce15-0410-9281-991c76e3d784
    committer: albright <albright@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/WeSay.Setup/Program.cs
+++ src/WeSay.Setup/Program.cs
@@ -1,51 +1,49 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 using Palaso.Reporting;
-
+using WeSay.Setup.Properties;
 
 namespace WeSay.Setup
 {
-	static class Program
+	internal static class Program
 	{
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-
 			SetupErrorHandling();
-			Palaso.Reporting.Logger.Init();
+			Logger.Init();
 
 			//bring in settings from any previous version
-			if (Properties.Settings.Default.NeedUpgrade)
+			if (Settings.Default.NeedUpgrade)
 			{
-				Properties.Settings.Default.Upgrade();
-				Properties.Settings.Default.NeedUpgrade = false;
+				Settings.Default.Upgrade();
+				Settings.Default.NeedUpgrade = false;
 			}
 
 			UsageReporter.AppNameToUseInDialogs = "WeSay";
 			UsageReporter.AppNameToUseInReporting = "WeSayConfig";
-			Palaso.Reporting.UsageReporter.RecordLaunch();
-			Palaso.Reporting.UsageReporter.DoTrivialUsageReport("usage@wesay.org", "Thank you for helping us test WeSay!", new int[] { 1, 5, 20, 40, 60, 80, 100 });
+			UsageReporter.RecordLaunch();
+			UsageReporter.DoTrivialUsageReport("usage@wesay.org",
+											   "Thank you for helping us test WeSay!",
+											   new int[] {1, 5, 20, 40, 60, 80, 100});
 
 			Application.Run(new AdminWindow(args));
 
-			Palaso.Reporting.Logger.WriteEvent("App Exiting Normally.");
-			Palaso.Reporting.Logger.ShutDown();
+			Logger.WriteEvent("App Exiting Normally.");
+			Logger.ShutDown();
 		}
 
 		private static void SetupErrorHandling()
 		{
-			 Palaso.Reporting.ErrorReport.EmailAddress = "issues@wesay.org";
-			 Palaso.Reporting.ErrorReport.AddStandardProperties();
-			 Palaso.Reporting.ExceptionHandler.Init();
+			ErrorReport.EmailAddress = "issues@wesay.org";
+			ErrorReport.AddStandardProperties();
+			ExceptionHandler.Init();
 		}
 	}
 }
