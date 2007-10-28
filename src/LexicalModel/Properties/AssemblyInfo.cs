925c616b8152088fd304b65075aa265d790c9645
John Hatton <hattonjohn@gmail.com>
2007-10-28 10:28:11 +0000

    git-svn-id: http://www.wesay.org/code/WeSay/trunk@979 b25276a9-ce15-0410-9281-991c76e3d784
    committer: hatton <hatton@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/LexicalModel/Properties/AssemblyInfo.cs
+++ src/LexicalModel/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]  //history: 1.0.0.0 untile 28 oct when I fixed <note>s
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: System.CLSCompliant(true)]
