48bf044023209901a7ef44162c84be256d342f62
Jonathan Anderson <jonathan_anderson@sil.org>
2008-07-02 09:38:23 +0000

    Moved two tests from DictionaryControlTests to WeSayListViewTests.
    git-svn-id: http://www.wesay.org/code/WeSay/trunk@1450 b25276a9-ce15-0410-9281-991c76e3d784
    
    committer: anderson <anderson@b25276a9-ce15-0410-9281-991c76e3d784>

--- src/LexicalTools.Tests/DictionaryControlTests.cs
+++ src/LexicalTools.Tests/DictionaryControlTests.cs
@@ -974,20 +974,6 @@ namespace WeSay.LexicalTools.Tests
 		}
 
 		[Test]
-		public void ClickOnWhiteSpaceToRightOfEntry_ThenKeyboardNavigate_CorrectEntrySelected()
-		{
-			ListViewTester l = new ListViewTester("_recordsListBox", _window);
-			MouseController mc = new MouseController(l);
-			KeyboardController kc = new KeyboardController(l);
-			l.Select(0);
-			Rectangle r = l.Properties.GetItemRect(1);
-			mc.Click(r.Right + 1, r.Top + 1);
-			kc.Press("{DOWN}");
-			kc.Release("{DOWN}");
-			Assert.AreEqual(2, l.Properties.SelectedIndices[0]);
-		}
-
-		[Test]
 		public void ClickOnWhiteSpaceToRightOfEntry_EntryAlreadySelected_DeleteButtonStaysEnabled()
 		{
 			ListViewTester l = new ListViewTester("_recordsListBox", _window);
@@ -998,20 +984,6 @@ namespace WeSay.LexicalTools.Tests
 			// move enough to not count as a double-click
 			mc.Click(r.Right + SystemInformation.DoubleClickSize.Width + 2, r.Top + 1);
 			Assert.IsTrue(b.Properties.Enabled);
-		}
-
-		[Test]
-		public void DoubleClickOnWhiteSpaceToRightOfEntry_EntryAlreadySelected_EntryStaysSelected()
-		{
-			ListViewTester l = new ListViewTester("_recordsListBox", _window);
-			ButtonTester b = new ButtonTester("_btnDeleteWord", _window);
-			MouseController mc = new MouseController(l);
-			Rectangle r = l.Properties.GetItemRect(0);
-			mc.Click(r.Right + 1, r.Top + 1);
-			// move enough to not confuse click with double-click
-			mc.DoubleClick(r.Right + SystemInformation.DoubleClickSize.Width + 2, r.Top + 1);
-			Assert.AreEqual(1, l.Properties.SelectedIndices.Count);
-			Assert.AreEqual(0, l.Properties.SelectedIndices[0]);
 		}
 //
 //        [Test]
---
+++ src/WeSay.UI.Tests/WeSayListViewTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+
+namespace WeSay.UI.Tests
+{
+	[TestFixture]
+	public class WeSayListViewTests : NUnitFormTest
+	{
+		private Form _window;
+		private WeSayListView _listView;
+		private List<string> _entries;
+
+		public override void Setup()
+		{
+			base.Setup();
+			_entries = new List<string>();
+			_entries.Add("Uno");
+			_entries.Add("Dos");
+			_entries.Add("Tres");
+			_listView = new WeSayListView();
+			_listView.Name = "_listView";
+			_listView.DataSource = _entries;
+			_window = new Form();
+			_window.Size = new Size(500, 500);
+			_window.Controls.Add(_listView);
+			_window.Show();
+		}
+
+		public override void TearDown()
+		{
+			_window.Dispose();
+			_listView.Dispose();
+			base.TearDown();
+		}
+
+		[Test]
+		public void ClickOnWhiteSpaceToRightOfEntry_ThenKeyboardNavigate_CorrectEntrySelected()
+		{
+			ListViewTester l = new ListViewTester("_listView", _window);
+			MouseController mc = new MouseController(l);
+			KeyboardController kc = new KeyboardController(l);
+			l.Select(0);
+			Rectangle r = l.Properties.GetItemRect(1);
+			mc.Click(r.Right + 1, r.Top + 1);
+			kc.Press("{DOWN}");
+			kc.Release("{DOWN}");
+			Assert.AreEqual(2, l.Properties.SelectedIndices[0]);
+		}
+
+		[Test]
+		public void DoubleClickOnWhiteSpaceToRightOfEntry_EntryAlreadySelected_EntryStaysSelected()
+		{
+			ListViewTester l = new ListViewTester("_listView", _window);
+			MouseController mc = new MouseController(l);
+			Rectangle r = l.Properties.GetItemRect(0);
+			mc.Click(r.Right + 1, r.Top + 1);
+			// move enough to not confuse click with double-click
+			mc.DoubleClick(r.Right + SystemInformation.DoubleClickSize.Width + 2, r.Top + 1);
+			Assert.AreEqual(1, l.Properties.SelectedIndices.Count);
+			Assert.AreEqual(0, l.Properties.SelectedIndices[0]);
+		}
+	}
+}
