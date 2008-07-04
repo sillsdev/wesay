20613967dd3a5728f5b60c3f8ecb59239c501fad
Tim Armstrong <tim_armstrong@sil.org>
2008-07-04 14:09:56 +0700

    Middle of integrating LiftUpdateService functionality into LiftRepository
    committer: tarmstrong <tim_armstrong@sil.org>

--- src/LexicalModel/LiftRepository.cs
+++ src/LexicalModel/LiftRepository.cs
@@ -131,10 +131,7 @@ namespace WeSay.LexicalModel
 				throw new ArgumentNullException("items");
 			}
 
-			foreach (LexEntry item in items)
-			{
-				SaveItem(item);
-			}
+			UpdateLiftFile(items);
 		}
 
 		public ResultSet<LexEntry> GetItemsMatching(Query query)
@@ -241,6 +238,269 @@ namespace WeSay.LexicalModel
 		{
 		}
 
+		//private const string s_updatePointFileName = "updatePoint";
+		private const int _checkFrequency = 10;
+		private int _commitCount;
+		private readonly LexEntryRepository _lexEntryRepository;
+		private DateTime _timeOfLastQueryForNewRecords;
+		//private bool _didFindDataInCacheNeedingRecovery = false;
 
+		private event EventHandler Updating;
+
+		private static string LiftDirectory
+		{
+			get { return Path.GetDirectoryName(_liftFilePath); }
+		}
+
+		//Don't think this is needed anymore TA 7-4-2008
+		///// <summary>
+		///// Give a chance to do incremental update if warranted
+		///// </summary>
+		///// <remarks>
+		///// If the caller doesn't know when actual comitts happen, that's ok.
+		///// Just call at reasonable intervals.
+		///// </remarks>
+		///// <param name="sender"></param>
+		///// <param name="e"></param>
+		//public void OnDataCommitted(object sender, EventArgs e)
+		//{
+		//    _commitCount++;
+		//    if (_commitCount < _checkFrequency)
+		//    {
+		//        return;
+		//    }
+		//    _commitCount = 0;
+		//    DoLiftUpdateNow(false);
+		//}
+
+		public void OnDataDeleted(object sender, EventArgs e)
+		{
+			LexEntry entry = (LexEntry)sender;
+			if (entry == null)
+			{
+				return;
+			}
+
+			LiftExporter exporter =
+					new LiftExporter( /*WeSayWordsProject.Project.GetFieldToOptionListNameDictionary(), */
+							MakeIncrementFileName(DateTime.UtcNow));
+			exporter.AddDeletedEntry(entry);
+			exporter.End();
+		}
+
+		private void UpdateLiftFile(IEnumerable<LexEntry> entriesToUpdate)
+		{
+			CreateIncrementFile(entriesToUpdate);
+			MergeIncrementFiles();
+		}
+
+		private void CreateIncrementFile(IEnumerable<LexEntry> entriesToUpdate)
+		{
+			//What is this for? TA 7-4-2008
+			//if (Updating != null)
+			//{
+			//    Updating.Invoke(this, null);
+			////}
+
+			//Can't get the count of an IEnumerable. What to do? TA -7-4-2008
+			//    LameProgressDialog dlg = null;
+			//    if (repositoryIds.Count > 50)
+			//    {
+			//        //TODO: if we think this will actually ever be called, clean this up with a real, delayed-visibility dialog
+			//        dlg =
+			//                new LameProgressDialog(
+			//                        string.Format("Doing incremental update of {0} records...",
+			//                                      repositoryIds.Count));
+			//        dlg.Show();
+			//        Application.DoEvents();
+			//    }
+
+				try
+				{
+					bool startOfFileWritten = false;
+					foreach (LexEntry entry in entriesToUpdate)
+					{
+						if (!startOfFileWritten)
+						{
+							LiftExporter exporter =
+								new LiftExporter( /*WeSayWordsProject.Project.GetFieldToOptionListNameDictionary(),*/
+												  MakeIncrementFileName(_timeOfLastQueryForNewRecords));
+							startOfFileWritten = true;
+						}
+						exporter.Add(entry);
+					}
+					if (startOfFileWritten)
+					{
+						exporter.End();
+					}
+
+					RecordUpdateTime(_timeOfLastQueryForNewRecords);  //Why do we need to call this? TA 7-4-2008
+				}
+				finally
+				{
+				//See comment above. TA 7-4-2008
+				//    if (dlg != null)
+				//    {
+				//        dlg.Close();
+				//        dlg.Dispose();
+				//    }
+				}
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <returns>false if it failed (and it would have already reported the error)</returns>
+		public static bool MergeIncrementFiles()
+		{
+			//merge the increment files
+
+			if (
+					SynchronicMerger.GetPendingUpdateFiles(WeSayWordsProject.Project.PathToLiftFile)
+							.Length > 0)
+			{
+				Logger.WriteEvent("Running Synchronic Merger");
+				try
+				{
+					SynchronicMerger merger = new SynchronicMerger();
+					WeSayWordsProject.Project.ReleaseLockOnLift();
+					merger.MergeUpdatesIntoFile(_liftFilePath);
+				}
+				catch (BadUpdateFileException error)
+				{
+					string contents = File.ReadAllText(error.PathToNewFile);
+					if (contents.Trim().Length == 0)
+					{
+						ErrorReport.ReportNonFatalMessage(
+								"It looks as though WeSay recently crashed while attempting to save.  It will try again to preserve your work, but you will want to check to make sure nothing was lost.");
+						File.Delete(error.PathToNewFile);
+					}
+					else
+					{
+						File.Move(error.PathToNewFile, error.PathToNewFile + ".bad");
+						ErrorReport.ReportNonFatalMessage(
+								"WeSay was unable to save some work you did in the previous session.  The work might be recoverable from the file {0}. The next screen will allow you to send a report of this to the developers.",
+								error.PathToNewFile + ".bad");
+						ErrorNotificationDialog.ReportException(error, null, false);
+					}
+					return false;
+				}
+				catch (Exception e)
+				{
+					throw new ApplicationException(
+							"Could not finish updating LIFT dictionary file.", e);
+				}
+				finally
+				{
+					WeSayWordsProject.Project.LockLift();
+				}
+			}
+			return true;
+		}
+
+		private static string MakeIncrementFileName(DateTime time)
+		{
+			while (true)
+			{
+				string timeString = time.ToString("yyyy'-'MM'-'dd'T'HH'-'mm'-'ss'-'FFFFFFF UTC");
+				string path = Path.Combine(LiftDirectory, timeString);
+				path += SynchronicMerger.ExtensionOfIncrementalFiles;
+				if (!File.Exists(path))
+				{
+					return path;
+				}
+				time = time.AddTicks(1);
+			}
+		}
+
+		/// <summary>
+		/// wierd name!
+		/// </summary>
+		public static void LiftIsFreshNow()
+		{
+			RecordUpdateTime(DateTime.UtcNow);
+		}
+
+		//What is this method for? TA 7-4-2008
+		protected static void RecordUpdateTime(DateTime time)
+		{
+			//// the resolution of the file modified time is a whole second on linux
+			//// so we need to set this to the ceiling of the time in seconds and then
+			//// wait until the actual time has passed this window
+			//int millisecondsLostInResolution = 1000 - time.Millisecond;
+			//time = time.AddMilliseconds(millisecondsLostInResolution);
+			//TimeSpan timeout = time - DateTime.UtcNow;
+			//if(timeout.Ticks > 0)
+			//{
+			//    Thread.Sleep(timeout);
+			//}
+			bool wasLocked = WeSayWordsProject.Project.LiftIsLocked;
+			if (wasLocked)
+			{
+				WeSayWordsProject.Project.ReleaseLockOnLift();
+			}
+			File.SetLastWriteTimeUtc(_liftFilePath, time);
+			//Debug.Assert(time == GetLastUpdateTime());
+			if (wasLocked)
+			{
+				WeSayWordsProject.Project.LockLift();
+			}
+		}
+		private static DateTime GetLastUpdateTime()
+		{
+			Debug.Assert(Directory.Exists(LiftDirectory));
+			return File.GetLastWriteTimeUtc(WeSayWordsProject.Project.PathToLiftFile);
+		}
+
+		public IList<RepositoryId> GetRecordsNeedingUpdateInLift()
+		{
+			DateTime last = GetLastUpdateTime();
+			_timeOfLastQueryForNewRecords = DateTime.UtcNow;
+			return _lexEntryRepository.GetItemsModifiedSince(last);
+		}
+
+		/// <summary>
+		/// Used to try again to get data out of the cache in case it crashed last time
+		/// We can be successful at saving on startup if the crash was somehow related to the UI (as in WS-554)
+		/// </summary>
+		public void RecoverUnsavedChangesOutOfCacheIfNeeded()
+		{
+			try
+			{
+				if (CacheManager.GetAssumeCacheIsFresh(WeSayWordsProject.Project.PathToCache))
+				{
+					return;
+					// setting permissions in the installer apparently was enough to mess this next line up on the sample data
+				}
+
+				IList<RepositoryId> records = GetRecordsNeedingUpdateInLift();
+				if (records.Count == 0)
+				{
+					return;
+				}
+
+				try
+				{
+					ErrorReport.ReportNonFatalMessage(
+							"It appears that WeSay did not exit normally last time.  WeSay will now attempt to recover the {0} records which were not saved.",
+							records.Count);
+					DoLiftUpdateNow(false);
+					//                    _didFindDataInCacheNeedingRecovery = true;
+					ErrorReport.ReportNonFatalMessage("Your work was successfully recovered.");
+				}
+				catch (Exception)
+				{
+					ErrorReport.ReportNonFatalMessage(
+							"Sorry, WeSay was unable to recover some of your work.");
+					WeSayWordsProject.Project.InvalidateCacheSilently();
+				}
+			}
+			catch (Exception)
+			{
+				ErrorReport.ReportNonFatalMessage(
+						"WeSay had a problem reading the cache.  It will now be rebuilt");
+				WeSayWordsProject.Project.InvalidateCacheSilently();
+			}
+		}
 	}
 }
