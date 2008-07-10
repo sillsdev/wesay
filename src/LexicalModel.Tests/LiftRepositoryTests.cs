20f51822bc9153d1484f5ae03763b539b56a1b02
Tim Armstrong <tim_armstrong@sil.org>
2008-07-10 11:08:38 +0700

    Added tests around constructor.
    committer: tarmstrong <tim_armstrong@sil.org>

--- src/LexicalModel.Tests/LiftRepositoryTests.cs
+++ src/LexicalModel.Tests/LiftRepositoryTests.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using NUnit.Framework;
+using WeSay.Data;
 using WeSay.Data.Tests;
 using WeSay.LexicalModel;
 
@@ -10,13 +11,55 @@ namespace WeSay.LexicalModel.Tests
 		public static string MakeFile()
 		{
 			string liftfileName = Path.GetTempFileName();
-			File.WriteAllText(liftfileName, @"<?xml version='1.0'?><lift version='0.12'></lift>");
+			File.WriteAllText(liftfileName,
+				@"<?xml version='1.0' encoding='utf-8'?>
+				<lift
+					version='0.12'
+					producer='WeSay 1.0.0.0'>
+					<entry
+						id='Sonne_c753f6cc-e07c-4bb1-9e3c-013d09629111'
+						dateCreated='2008-07-01T06:29:23Z'
+						dateModified='2008-07-01T06:29:57Z'
+						guid='c753f6cc-e07c-4bb1-9e3c-013d09629111'>
+						<lexical-unit>
+							<form
+								lang='v'>
+								<text>Sonne</text>
+							</form>
+						</lexical-unit>
+						<sense
+							id='33d60091-ba96-4204-85fe-9d15a24bd5ff'>
+							<trait
+								name='SemanticDomainDdp4'
+								value='1 Universe, creation' />
+						</sense>
+					</entry>
+				</lift>");
 			return liftfileName;
 		}
 	}
 
 	[TestFixture]
 	public class LiftRepositoryStateUnitializedTests:IRepositoryStateUnitializedTests<LexEntry>
+	{
+		private string _persistedFilePath;
+		[SetUp]
+		public void Setup()
+		{
+			this._persistedFilePath = Path.GetRandomFileName();
+			this.RepositoryUnderTest = new LiftRepository(this._persistedFilePath);
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			RepositoryUnderTest.Dispose();
+			File.Delete(this._persistedFilePath);
+		}
+	}
+
+	[TestFixture]
+	public class LiftRepositoryCreatedFromPersistedData : IRepositoryPopulateFromPersistedTests<LexEntry>
 	{
 		private string _persistedFilePath;
 		[SetUp]
@@ -32,6 +75,18 @@ namespace WeSay.LexicalModel.Tests
 			RepositoryUnderTest.Dispose();
 			File.Delete(this._persistedFilePath);
 		}
+
+		[Test]
+		public override void LastModified_IsSetToPersistedDatasLastChangedTime()
+		{
+			Assert.Fail();
+		}
+
+		protected override void RepopulateRepositoryFromPersistedData()
+		{
+			RepositoryUnderTest.Dispose();
+			RepositoryUnderTest = new LiftRepository(_persistedFilePath);
+		}
 	}
 
 	[TestFixture]
@@ -41,7 +96,7 @@ namespace WeSay.LexicalModel.Tests
 		[SetUp]
 		public void Setup()
 		{
-			this._persistedFilePath = LiftFileInitializer.MakeFile();
+			this._persistedFilePath = Path.GetRandomFileName();
 			this.RepositoryUnderTest = new LiftRepository(this._persistedFilePath);
 		}
 
@@ -58,15 +113,15 @@ namespace WeSay.LexicalModel.Tests
 			RepositoryUnderTest = new LiftRepository(_persistedFilePath);
 		}
 
-		public override void SaveItem_ItemHasBeenPersisted()
-		{
+		//public override void SaveItem_ItemHasBeenPersisted()
+		//{
+		//    Assert.Fail();
+		//}
 
-		}
-
-		public override void SaveItems_ItemHasBeenPersisted()
-		{
-			Assert.Fail();
-		}
+		//public override void SaveItems_ItemHasBeenPersisted()
+		//{
+		//    Assert.Fail();
+		//}
 	}
 
 	[TestFixture]
@@ -76,7 +131,7 @@ namespace WeSay.LexicalModel.Tests
 		[SetUp]
 		public void Setup()
 		{
-			this._persistedFilePath = LiftFileInitializer.MakeFile();
+			this._persistedFilePath = Path.GetRandomFileName();
 			this.RepositoryUnderTest = new LiftRepository(this._persistedFilePath);
 		}
 
@@ -101,7 +156,7 @@ namespace WeSay.LexicalModel.Tests
 		[SetUp]
 		public void Setup()
 		{
-			this._persistedFilePath = LiftFileInitializer.MakeFile();
+			this._persistedFilePath = Path.GetRandomFileName();
 			this.RepositoryUnderTest = new LiftRepository(this._persistedFilePath);
 		}
 
