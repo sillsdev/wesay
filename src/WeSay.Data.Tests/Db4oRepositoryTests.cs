f9785cd10fd335bd8852569c1c6e909b9e880850
Eric Albright <eric_albright@sil.org>
2008-07-24 15:12:51 +0700

    Add Db4oRepositoryCreatedFromPersistedData tests
    committer: albright <albright@wesay.org>

--- src/WeSay.Data.Tests/Db4oRepositoryTests.cs
+++ src/WeSay.Data.Tests/Db4oRepositoryTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using WeSay.Data;
@@ -22,6 +24,72 @@ namespace WeSay.Data.Tests
 			File.Delete(_name);
 		}
 
+	}
+
+	[TestFixture]
+	public class Db4oRepositoryCreatedFromPersistedData : IRepositoryPopulateFromPersistedTests<TestItem>
+	{
+		private string _persistedFilePath;
+		[SetUp]
+		public void Setup()
+		{
+			this._persistedFilePath = Path.GetTempFileName();
+			RepositoryUnderTest = new Db4oRepository<TestItem>(this._persistedFilePath);
+			TestItem item = RepositoryUnderTest.CreateItem();
+			item.StoredInt = 5;
+			item.StoredString = "Sonne";
+			RepositoryUnderTest.SaveItem(item);
+
+			CreateNewRepositoryFromPersistedData();
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			RepositoryUnderTest.Dispose();
+			File.Delete(this._persistedFilePath);
+		}
+
+		[Test]
+		public override void SaveItem_LastModifiedIsChangedToLaterTime()
+		{
+			SetState();
+			DateTime modifiedTimePreSave = RepositoryUnderTest.LastModified;
+			RepositoryUnderTest.SaveItem(Item);
+			Assert.Greater(RepositoryUnderTest.LastModified, modifiedTimePreSave);
+		}
+
+		[Test]
+		public override void SaveItems_LastModifiedIsChangedToLaterTime()
+		{
+			SetState();
+			List<TestItem> itemsToSave = new List<TestItem>();
+			itemsToSave.Add(Item);
+			DateTime modifiedTimePreSave = RepositoryUnderTest.LastModified;
+			RepositoryUnderTest.SaveItems(itemsToSave);
+			Assert.Greater(RepositoryUnderTest.LastModified, modifiedTimePreSave);
+		}
+
+		[Test]
+		public override void LastModified_IsSetToMostRecentItemInPersistedDatasLastModifiedTime()
+		{
+		}
+
+		[Test]
+		public override void GetItemMatchingQuery_QueryWithShow_ReturnsAllItemsAndFieldsMatchingQuery()
+		{
+			SetState();
+			Query query = new Query(typeof(TestItem)).Show("StoredString");
+			ResultSet<TestItem> resultsOfQuery = RepositoryUnderTest.GetItemsMatching(query);
+			Assert.AreEqual(1, resultsOfQuery.Count);
+			Assert.AreEqual("Sonne", resultsOfQuery[0]["StoredString"]);
+		}
+
+		protected override void CreateNewRepositoryFromPersistedData()
+		{
+			RepositoryUnderTest.Dispose();
+			RepositoryUnderTest = new Db4oRepository<TestItem>(_persistedFilePath);
+		}
 	}
 
 	[TestFixture]
