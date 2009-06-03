38a09dd47ce00835f275733be01ab8da59c31e35
Cambell Prince <cambell_prince@sil.org>
2009-06-03 13:05:03 +0700

    Reenable error handling

--- src/WeSay.App/WeSayApp.cs
+++ src/WeSay.App/WeSayApp.cs
@@ -53,7 +53,7 @@ namespace WeSay.App
 			}
 			OsCheck();
 			Logger.Init();
-			// FIXME: SetupErrorHandling();
+			SetupErrorHandling();
 			//problems with user.config: http://blogs.msdn.com/rprabhu/articles/433979.aspx
 
 			//bring in settings from any previous version
