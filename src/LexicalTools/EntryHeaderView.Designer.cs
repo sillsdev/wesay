9f8743260665fbce2c59b4f1f3835c2ec9b64585
Tim Armstrong <tim_armstrong@sil.org>
2010-09-22 14:12:13 +0700

    Changed color of EntryHeaderView. this is needed so that appropriate events are fired when changing to WeSay green.

--- src/LexicalTools/EntryHeaderView.Designer.cs
+++ src/LexicalTools/EntryHeaderView.Designer.cs
@@ -48,6 +48,7 @@
 			//
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
 			this.Controls.Add(this._entryPreview);
 			this.Name = "EntryHeaderView";
 			this.Size = new System.Drawing.Size(527, 129);
