f7318fdec8e9aaabc1c3c1339c2dcb95400aadc9
Tim Armstrong <tim_armstrong@sil.org>
2010-11-01 15:49:13 +0700

    Addendum to revision 2661

---
+++ src/WeSay.Project/ConfigMigration/WritingSystems/WritingSystemsFromLiftCreator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using WeSay.LexicalModel.Foundation;
+
+namespace WeSay.Project.ConfigMigration.WritingSystems
+{
+	class WritingSystemsFromLiftCreator
+	{
+		private string _pathToProjectFolder;
+
+		public WritingSystemsFromLiftCreator(string pathToProjectFolder)
+		{
+			_pathToProjectFolder = pathToProjectFolder;
+		}
+
+		public void CreateNonExistantWritingSystemsFoundInLift(string pathToLiftFile)
+		{
+			if (File.Exists(pathToLiftFile))
+			{
+				string pathToLdmlWritingSystemsFolder = BasilProject.GetPathToLdmlWritingSystemsFolder(_pathToProjectFolder);
+				WritingSystemCollection writingSystems = new WritingSystemCollection();
+				writingSystems.Load(pathToLdmlWritingSystemsFolder);
+				using (XmlReader reader = XmlReader.Create(pathToLiftFile))
+				{
+					while (reader.Read())
+					{
+						if (reader.MoveToAttribute("lang"))
+						{
+							if (!writingSystems.Keys.Contains(reader.Value))
+							{
+								writingSystems.AddSimple(reader.Value);
+							}
+						}
+					}
+				}
+				writingSystems.Write(pathToLdmlWritingSystemsFolder);
+			}
+		}
+	}
+}
