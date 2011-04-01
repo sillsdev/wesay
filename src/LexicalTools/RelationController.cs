391e4500a63b33b70b185d51c3a8f16b60fb4afd
John Hatton <hattonjohn@gmail.com>
2011-04-01 21:06:00 +1000

    Fix for WS-33976, newly created "baseforms" were not getting saved.

--- src/LexicalTools/RelationController.cs
+++ src/LexicalTools/RelationController.cs
@@ -77,6 +77,7 @@ namespace WeSay.LexicalTools
 		{
 			LexEntry newGuy = CreateNewLexEntry(e);
 			WritingSystem writingSystem = GetWritingSystemFromField();
+			_lexEntryRepository.SaveItem(newGuy);
 			_lexEntryRepository.NotifyThatLexEntryHasBeenUpdated(newGuy);
 			_resultSet = _lexEntryRepository.GetAllEntriesSortedByLexicalFormOrAlternative(writingSystem);
 			e.NewlyCreatedItem = GetRecordTokenFromLexEntry(newGuy);
