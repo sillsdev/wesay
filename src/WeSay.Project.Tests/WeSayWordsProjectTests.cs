4c20902d8909703a9beee8a7649a232833d31a22
Tim Armstrong <tim_armstrong@sil.org>
2011-04-19 09:44:24 +0700

    Cleaned up some tests around what used to be the writingsystemcollection and is now the ldmlinfolderwritingsystemrepository

--- src/WeSay.Project.Tests/WeSayWordsProjectTests.cs
+++ src/WeSay.Project.Tests/WeSayWordsProjectTests.cs
@@ -452,5 +452,32 @@ namespace WeSay.Project.Tests
 				Assert.That(project.UiOptions.Language, Is.EqualTo("en"));
 			}
 		}
+
+		[Test]
+		//This test was formerly part of the LdmlInFolderWritingSystemCollectionTests and needs to be changed to test that a new WeSayProject comes with an "UnknownVernacular" writingsystem 4/19/2011
+		[Category("WritingSystemRefactor")]
+		public void ProjectCreation_WritingSystemCollection_HasUnknownVernacular()
+		{
+			throw new NotImplementedException();
+			//string pathToWritingSystemsFolder
+			//Assert.Fail("WeSayProject Tests should perhaps implement this... cjh");
+
+			//IWritingSystemRepository c = new LdmlInFolderWritingSystemRepository(pathToWritingSystemsFolder);
+			//Assert.IsNotNull(c.Get(WritingSystemInfo.IdForUnknownVernacular));
+		}
+
+		[Test]
+		//This test was formerly part of the LdmlInFolderWritingSystemCollectionTests and needs to be changed to test that a new WeSayProject comes with an "UnknownAnalysis" writingsystem 4/19/2011
+		[Category("WritingSystemRefactor")]
+		public void WritingSystemCollection_HasUnknownAnalysis()
+		{
+			throw new NotImplementedException();
+			//using (var e = new TestEnvironment())
+			//{
+			//    Assert.Fail("WeSayProject Tests should perhaps implement this... cjh");
+			//    IWritingSystemRepository c = new LdmlInFolderWritingSystemRepository(e.PathToWritingSystemsFolder);
+			//    Assert.IsNotNull(c.Get(WritingSystemInfo.IdForUnknownAnalysis));
+			//}
+		}
 	}
 }
