0c695cb485b75371bef6fc6c71f7c46112dcddc6
Tim Armstrong <tim_armstrong@sil.org>
2011-04-27 14:28:15 +0700

    Updated WritingSystemPrefsFileContent class to make creating writingsystemprefx.xml files conatining more than one writingystem easier.

--- src/WeSay.Project.Tests/ConfigMigration/WritingSystem/WritingSystemPrefsFileContent.cs
+++ src/WeSay.Project.Tests/ConfigMigration/WritingSystem/WritingSystemPrefsFileContent.cs
@@ -4,7 +4,7 @@ namespace WeSay.Project.Tests.ConfigMigration.WritingSystem
 {
 	public class WritingSystemPrefsFileContent
 	{
-		public static string SingleWritingSystem(
+		public static string GetSingleWritingSystemXml(
 			string id,
 			string abbreviation,
 			string sortUsing,
@@ -24,10 +24,7 @@ namespace WeSay.Project.Tests.ConfigMigration.WritingSystem
 				sortRulesXml = String.Format("<CustomSortRules>{0}</CustomSortRules>", customSortRules);
 			}
 			return String.Format(
-				@"<?xml version='1.0' encoding='utf-8'?>
-<WritingSystemCollection>
-  <members>
-	<WritingSystem>
+				@"<WritingSystem>
 	  <Abbreviation>{0}</Abbreviation>
 	  {1}
 	  <FontName>{2}</FontName>
@@ -39,12 +36,38 @@ namespace WeSay.Project.Tests.ConfigMigration.WritingSystem
 	  <RightToLeft>{8}</RightToLeft>
 	  <SortUsing>{9}</SortUsing>
 	  <SpellCheckingId>{10}</SpellCheckingId>
-	</WritingSystem>
-  </members>
-</WritingSystemCollection>".Replace("'", "\""),
+	</WritingSystem>".Replace("'", "\""),
 				abbreviation, sortRulesXml, fontName, fontSize, id, isAudio,
 				isUnicode, keyboard, rightToleft, sortUsing, spellCheckingId
 				);
+		}
+
+		public static string SingleWritingSystem(
+			string id,
+			string abbreviation,
+			string sortUsing,
+			string customSortRules,
+			string fontName,
+			int fontSize,
+			bool rightToleft,
+			string spellCheckingId,
+			string keyboard,
+			bool isUnicode,
+			bool isAudio
+			)
+		{
+			string singleWritingsystemXml = GetSingleWritingSystemXml(id, abbreviation, sortUsing, customSortRules,
+																	  fontName, fontSize, rightToleft, spellCheckingId,
+																	  keyboard, isUnicode, isAudio);
+			return WrapWritingSystemXmlWithCollectionXml(singleWritingsystemXml);
+		}
+
+		public static string WrapWritingSystemXmlWithCollectionXml(string writingSystemXml)
+		{
+			return  "<?xml version='1.0' encoding='utf-8'?><WritingSystemCollection><members>" +
+					writingSystemXml                                                           +
+					"</members></WritingSystemCollection>";
+
 		}
 
 		public static string SingleWritingSystemForLanguage(string language)
@@ -54,5 +77,17 @@ namespace WeSay.Project.Tests.ConfigMigration.WritingSystem
 			);
 		}
 
+
+
+		public static string TwoWritingSystems(string language1, string language2)
+		{
+			string ws1Xml = GetSingleWritingSystemXml(
+				language1, language1, "", "", "Arial", 11, false, language1, language1, true, false
+			);
+			string ws2Xml = GetSingleWritingSystemXml(
+				language2, language2, "", "", "Arial", 11, false, language2, language2, true, false
+			);
+			return WrapWritingSystemXmlWithCollectionXml(String.Concat(ws1Xml, ws2Xml));
+		}
 	}
 }
