d62839061d239c9a27120bc6d95cde95c71565fc
Cambell Prince <cambell_prince@sil.org>
2011-05-02 09:49:55 +0700

    Fixed WeSayProject tests use of TempFile

--- src/WeSay.Project.Tests/WeSayWordsProjectTests.cs
+++ src/WeSay.Project.Tests/WeSayWordsProjectTests.cs
@@ -509,7 +509,7 @@ namespace WeSay.Project.Tests
 			{
 				//setting up a minimal WeSay project with an old writingsystemprefs.xml file
 				var project = new WeSayWordsProject();
-				var wsPrefsFile = new TempFile(projectDirectory);
+				var wsPrefsFile = projectDirectory.GetNewTempFile(true);
 				string liftFilePath = Path.Combine(projectDirectory.Path, "test.lift");
 				wsPrefsFile.MoveTo(Path.Combine(projectDirectory.Path, "WritingSystemPrefs.xml"));
 				File.Copy(Path.Combine(project.ApplicationTestDirectory, "PRETEND.WeSayConfig"), Path.Combine(projectDirectory.Path, "test.WeSayConfig"));
