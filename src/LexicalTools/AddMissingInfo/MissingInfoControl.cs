91009e8823af9501e802f038d75a4c1095f54cb6
Tim Armstrong <tim_armstrong@sil.org>
2011-06-29 14:13:35 +0700

    WS-34069: In the missing info control the next item in the "todo" list is now selected on mouse down rather than click event.

--- src/LexicalTools/AddMissingInfo/MissingInfoControl.cs
+++ src/LexicalTools/AddMissingInfo/MissingInfoControl.cs
@@ -482,7 +482,7 @@ namespace WeSay.LexicalTools.AddMissingInfo
 			SetCurrentRecordToPrevious();
 		}
 
-		private void OnBtnNextWordClick(object sender, EventArgs e)
+		private void OnBtnNextWordClick(object sender, MouseEventArgs e)
 		{
 			SetCurrentRecordToNext();
 		}
