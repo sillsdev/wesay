dba67e59610480c73f22b2f88ee0ea36625a9014
John Hatton <hattonjohn@gmail.com>
2011-07-07 10:30:56 -0700

    FIx WS-34091: Give a better message when creation of a font fails.

--- src/LexicalModel/Foundation/WritingSystemInfo.cs
+++ src/LexicalModel/Foundation/WritingSystemInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using Palaso.Reporting;
 using Palaso.WritingSystems;
 
 namespace WeSay.LexicalModel.Foundation
@@ -8,7 +10,17 @@ namespace WeSay.LexicalModel.Foundation
 		public static Font CreateFont(WritingSystemDefinition writingSystem)
 		{
 			float size = writingSystem.DefaultFontSize > 0 ? writingSystem.DefaultFontSize : 12;
-			return new Font(writingSystem.DefaultFontName, size);
+			try
+			{
+				return new Font(writingSystem.DefaultFontName, size);
+			}
+			catch (Exception e) //I could never get the above to fail, but a user did (WS-34091), so now we catch it here.
+			{
+				ErrorReport.NotifyUserOfProblem(new ShowOncePerSessionBasedOnExactMessagePolicy(),
+					e,
+					"There is something wrong with the font {0} on this computer. Try re-installing it. Meanwhile, the font {1} will be used instead.", writingSystem.DefaultFontName, SystemFonts.DefaultFont.Name);
+				return new Font(SystemFonts.DefaultFont.Name, size);
+			}
 		}
 	}
 }
