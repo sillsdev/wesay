24497e1521cebed74444a0bb5f63341b5127eafe
Chris Hubbard <chris_hubbard@sil.org>
2014-01-08 14:11:23 -0500

    Include MercurialExtensions (TC Config Change)

--- build/buildupdate.win.sh
+++ build/buildupdate.win.sh
@@ -16,7 +16,7 @@
 #     VCS:  []
 #     clean: false
 #     revision: latest.lastSuccessful
-#     paths: {"Chorus.exe"=>"lib/Release", "Chorus.pdb"=>"lib/Release", "LibChorus.dll"=>"lib/Release", "LibChorus.pdb"=>"lib/Release", "ChorusMerge.exe"=>"lib/Release", "ChorusMerge.pdb"=>"lib/Release", "LibChorus.TestUtilities.dll"=>"lib/Release", "LibChorus.TestUtilities.pdb"=>"lib/Release", "Mercurial.zip"=>"lib/Release"}
+#     paths: {"Chorus.exe"=>"lib/Release", "Chorus.pdb"=>"lib/Release", "LibChorus.dll"=>"lib/Release", "LibChorus.pdb"=>"lib/Release", "ChorusMerge.exe"=>"lib/Release", "ChorusMerge.pdb"=>"lib/Release", "LibChorus.TestUtilities.dll"=>"lib/Release", "LibChorus.TestUtilities.pdb"=>"lib/Release", "Mercurial.zip"=>"lib/Release", "MercurialExtensions/**"=>"MercurialExtensions"}
 # [1] build: chorus-win32-master Continuous (bt2)
 #     project: Chorus
 #     URL: http://build.palaso.org/viewType.html?buildTypeId=bt2
@@ -44,10 +44,12 @@
 #     VCS:  []
 #     clean: false
 #     revision: latest.lastSuccessful
-#     paths: {"icu*.dll"=>"lib/Release", "icu*.config"=>"lib/Release"}
+#     paths: {"icu*.dll"=>"lib/Release"}
 
 # make sure output directories exist
 mkdir -p ../lib/Release
+mkdir -p ../MercurialExtensions
+mkdir -p ../MercurialExtensions/fixutf8
 mkdir -p ../lib/common
 
 # download artifact dependencies
@@ -60,6 +62,24 @@ curl -L -o ../lib/Release/ChorusMerge.pdb http://build.palaso.org/guestAuth/repo
 curl -L -o ../lib/Release/LibChorus.TestUtilities.dll http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/LibChorus.TestUtilities.dll
 curl -L -o ../lib/Release/LibChorus.TestUtilities.pdb http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/LibChorus.TestUtilities.pdb
 curl -L -o ../lib/Release/Mercurial.zip http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/Mercurial.zip
+curl -L -o ../MercurialExtensions/.guidsForInstaller.xml http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/.guidsForInstaller.xml
+curl -L -o ../MercurialExtensions/Dummy.txt http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/Dummy.txt
+curl -L -o ../MercurialExtensions/fixutf8/.gitignore http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/.gitignore
+curl -L -o ../MercurialExtensions/fixutf8/.guidsForInstaller.xml http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/.guidsForInstaller.xml
+curl -L -o ../MercurialExtensions/fixutf8/.hg_archival.txt http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/.hg_archival.txt
+curl -L -o ../MercurialExtensions/fixutf8/.hgignore http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/.hgignore
+curl -L -o ../MercurialExtensions/fixutf8/README. http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/README.
+curl -L -o ../MercurialExtensions/fixutf8/buildcpmap.py http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/buildcpmap.py
+curl -L -o ../MercurialExtensions/fixutf8/cpmap.pyc http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/cpmap.pyc
+curl -L -o ../MercurialExtensions/fixutf8/fixutf8.py http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/fixutf8.py
+curl -L -o ../MercurialExtensions/fixutf8/fixutf8.pyc http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/fixutf8.pyc
+curl -L -o ../MercurialExtensions/fixutf8/fixutf8.pyo http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/fixutf8.pyo
+curl -L -o ../MercurialExtensions/fixutf8/osutil.py http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/osutil.py
+curl -L -o ../MercurialExtensions/fixutf8/osutil.pyc http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/osutil.pyc
+curl -L -o ../MercurialExtensions/fixutf8/osutil.pyo http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/osutil.pyo
+curl -L -o ../MercurialExtensions/fixutf8/win32helper.py http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/win32helper.py
+curl -L -o ../MercurialExtensions/fixutf8/win32helper.pyc http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/win32helper.pyc
+curl -L -o ../MercurialExtensions/fixutf8/win32helper.pyo http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/MercurialExtensions/fixutf8/win32helper.pyo
 curl -L -o ../lib/common/Autofac.dll http://build.palaso.org/guestAuth/repository/download/bt2/latest.lastSuccessful/Autofac.dll
 curl -L -o ../lib/common/L10NSharp.dll http://build.palaso.org/guestAuth/repository/download/bt196/latest.lastSuccessful/L10NSharp.dll
 curl -L -o ../lib/common/L10NSharp.pdb http://build.palaso.org/guestAuth/repository/download/bt196/latest.lastSuccessful/L10NSharp.pdb
@@ -80,4 +100,3 @@ curl -L -o ../lib/Release/icu.net.dll http://build.palaso.org/guestAuth/reposito
 curl -L -o ../lib/Release/icudt40.dll http://build.palaso.org/guestAuth/repository/download/bt14/latest.lastSuccessful/icudt40.dll
 curl -L -o ../lib/Release/icuin40.dll http://build.palaso.org/guestAuth/repository/download/bt14/latest.lastSuccessful/icuin40.dll
 curl -L -o ../lib/Release/icuuc40.dll http://build.palaso.org/guestAuth/repository/download/bt14/latest.lastSuccessful/icuuc40.dll
-curl -L -o ../lib/Release/icu.net.dll.config http://build.palaso.org/guestAuth/repository/download/bt14/latest.lastSuccessful/icu.net.dll.config
