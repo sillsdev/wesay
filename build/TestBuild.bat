306cdc6181edefceed50cc2b02cf3ea048751e13
Chris Hubbard <chris_hubbard@sil.org>
2014-01-09 15:56:40 -0500

    Support both VS Express 2010 and VS Pro 2010

--- build/TestBuild.bat
+++ build/TestBuild.bat
@@ -1,8 +1,18 @@
 @echo off
 @if "%INCLUDE%" == "" (
-	echo "Setting up VS Tools..."
-	@call "%VS110COMNTOOLS%vsvars32.bat"
+	if not "%VS100COMNTOOLS%" == "" (
+		echo "Setting up VisualStudio 2010 Tools..."
+		@call "%VS100COMNTOOLS%vsvars32.bat"
+		goto build
+	)
+
+	if not "%VS110COMNTOOLS%" == "" (
+		echo "Setting up Visual Studio Express 2010 Tools..."
+		@call "%VS110COMNTOOLS%vsvars32.bat"
+		goto build
+	)
 )
+:build
 @echo on
 
-msbuild "/target:Clean;Compile" /p:Configuration=Release /p:RootDir=..  /p:BUILD_NUMBER="0.0.1.abcd" build.win.proj
+msbuild "/target:Clean;Compile" /p:Configuration=Release /p:Platform=x86 /p:RootDir=..  /p:BUILD_NUMBER="0.0.1.abcd" build.win.proj
