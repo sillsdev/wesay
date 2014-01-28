a32a9ae82838df8a4618af03a5a5fafc7bed787a
Chris Hubbard <chris_hubbard@sil.org>
2014-01-28 15:55:05 -0500

    Change to run anywhere and use mono-sil
    Change-Id: I762f358dbfb634798191ab9e388a07f6bd1f3a12

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-if [ "$1" == "" ]
-then
-	export BUILD=Release
-else
-	export BUILD="$1"
-fi
-
-xbuild "/target:Clean;Compile" /p:Configuration="${BUILD}" /p:RootDir=..  /p:BUILD_NUMBER="0.0.1.abcd" build.mono.proj
+cd "$(dirname "$0")"/..
+. environ
+cd  build
+xbuild "/target:Clean;Compile" /p:Configuration="${1:-Release}" /p:RootDir=..  /p:BUILD_NUMBER="0.0.1.abcd" build.mono.proj
