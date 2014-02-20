1cc13dd37faa4ec9ed2d2094ee0aaa4c8f96c667
Chris Hubbard <chris_hubbard@sil.org>
2014-02-20 17:06:56 -0500

    Add vagrant support for testing
    Change-Id: Ia16b1502d1345d1bb932e040bf035e4228802310

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+apt-get -y install python-software-properties pkg-config
+add-apt-repository 'deb http://packages.sil.org/ubuntu precise main'
+add-apt-repository 'deb http://packages.sil.org/ubuntu precise-experimental main'
+wget -O - http://packages.sil.org/sil.gpg | sudo apt-key add -
+apt-get update
+apt-get -y install geckofx xulrunner-geckofx mono-sil mono-basic-sil libgdiplus-sil gtk-sharp2-sil libimage-exiftool-perl git curl
