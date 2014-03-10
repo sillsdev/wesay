46eee459ffd83644700ce2a21ea32656918b65b8
Chris Hubbard <chris_hubbard@sil.org>
2014-03-10 16:38:29 -0400

    Vagrant config tweaks
    * After further review, install-buildupdate.sh should have sudo
      just on lines that need it
    * Change to fail if plugins are not installed
    * Add dependency on vagrant-vbguest plugin (so that correct)
      VirtualBox Guest Additions are installed.
    
    Change-Id: I3bff12b35a0603f46c94344cdc6e8d87cdebcd47

--- vagrant/install-buildupdate.sh
+++ vagrant/install-buildupdate.sh
@@ -1,9 +1,9 @@
-apt-get -y install ruby1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev
-gem install bundler
+sudo apt-get -y install ruby1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev
+sudo gem install bundler
 mkdir -p ~/src
 cd ~/src
 git clone https://github.com/chrisvire/BuildUpdate
 cd ~/src/BuildUpdate
 bundle install
-bundle exec gem pristine nokogiri
+sudo bundle exec gem pristine nokogiri
 echo "~/src/BuildUpdate ready"
