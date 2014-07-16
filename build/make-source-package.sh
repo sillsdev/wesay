c575645eeba5ca2533c0dec3f434b6a845ba6618
Chris Hubbard <chris_hubbard@sil.org>
2014-07-16 17:17:14 -0400

    Fix Ubuntu packaging to allow semantic versioning suffix
    The build scripts assumed that the third part of the version number
    was always a number.  For Ubuntu, we want to add ~alphaX to indicate
    that it is a alpha release.
    
    Change-Id: I53a40c10cc8ee065035ed73918c8a7b5b24fc0f0

--- build/make-source-package.sh
+++ build/make-source-package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 
 XBUILD=${MONO_PREFIX:-/usr}/bin/xbuild
-BUILD_COUNTER=${1:-1234}
+VERSION_SUFFIX=${1:-1234}
+BUILD_COUNTER=(${VERSION_SUFFIX//[~+-]/ })
 
 up()
 {
@@ -14,27 +15,27 @@ bld()
 	(cd build && $XBUILD /target:${1?} \
 		/property:Configuration=${2?} \
 		/property:RootDir=$(cd .. && pwd) \
-		/property:BUILD_NUMBER="0.0.$BUILD_COUNTER.abcd" build.mono.proj)
+		/property:BUILD_NUMBER="0.0.$3.abcd" build.mono.proj)
 }
 
 cd ../libpalaso
 up
-bld SourcePackage DebugMono
+bld SourcePackage DebugMono ${BUILD_COUNTER}
 cd $OLDPWD
 
 cd ../chorus
 mkdir -p externals
 cp -p ../libpalaso/output/DebugMono/libpalaso-2.5.${BUILD_COUNTER}.0.tar.gz externals/
 up
-bld SourcePackage DebugMono
-cd $OLDPWD
+bld SourcePackage DebugMono ${BUILD_COUNTER}
 
+cd $OLDPWD
 mkdir -p externals
 cp -p ../chorus/output/DebugMono/chorus-2.4.${BUILD_COUNTER}.0.tar.gz externals/
 up
-bld SourcePackage Release
+bld SourcePackage Release ${VERSION_SUFFIX}
 
 rm -rf lib/Debug
 rm -rf lib/Release/[Gg]ecko*
 
-bld Installer Release
+bld Installer Release ${VERSION_SUFFIX}
