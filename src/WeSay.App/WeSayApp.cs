e4e8a37de480d623fb98ed94527338110fe57bd3
Chris Hubbard <chris_hubbard@sil.org>
2014-09-24 10:00:02 -0400

    Revert "Add ENV variable to control XulRunner Init"
    This reverts commit defd70b39b40e09caeadcdd6e46f77af9f018c2b.
    
    I believe the correct solution to WS-84 is to use
    MONO_DEBUG=explicit-null-checks and we don't need this test code
    anymore.
    
    Change-Id: I0698fbcdca0d2393bf91c26fb750b38a8907e217

--- src/WeSay.App/WeSayApp.cs
+++ src/WeSay.App/WeSayApp.cs
@@ -99,22 +99,16 @@ namespace WeSay.App
 				string geckoBrowserOption = Environment.GetEnvironmentVariable("WESAY_USE_GECKO") ?? String.Empty;
 				WeSayWordsProject.GeckoOption = !(geckoBrowserOption == String.Empty  || geckoBrowserOption.Equals("0", StringComparison.OrdinalIgnoreCase));
 #if __MonoCS__
-				string initXulRunnerOption = Environment.GetEnvironmentVariable("WESAY_INIT_XULRUNNER") ?? String.Empty;
-				bool initXulRunner = (initXulRunnerOption == String.Empty  || !initXulRunnerOption.Equals("0", StringComparison.OrdinalIgnoreCase));
-				if (initXulRunner || WeSayWordsProject.GeckoOption)
-				{
-					// Initialize XULRunner - required to use the geckofx WebBrowser Control (GeckoWebBrowser).
-					string xulRunnerLocation = XULRunnerLocator.GetXULRunnerLocation();
-					if (String.IsNullOrEmpty(xulRunnerLocation))
-						throw new ApplicationException("The XULRunner library is missing or has the wrong version");
-					string librarySearchPath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH") ?? String.Empty;
-					if (!librarySearchPath.Contains(xulRunnerLocation))
-						throw new ApplicationException("LD_LIBRARY_PATH must contain " + xulRunnerLocation);
+				// Initialize XULRunner - required to use the geckofx WebBrowser Control (GeckoWebBrowser).
+				string xulRunnerLocation = XULRunnerLocator.GetXULRunnerLocation();
+				if (String.IsNullOrEmpty(xulRunnerLocation))
+					throw new ApplicationException("The XULRunner library is missing or has the wrong version");
+				string librarySearchPath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH") ?? String.Empty;
+				if (!librarySearchPath.Contains(xulRunnerLocation))
+					throw new ApplicationException("LD_LIBRARY_PATH must contain " + xulRunnerLocation);
 
-					Xpcom.Initialize(xulRunnerLocation);
-					GeckoPreferences.User["gfx.font_rendering.graphite.enabled"] = true;
-					Console.WriteLine("XulRunner Initialized");
-				}
+				Xpcom.Initialize(xulRunnerLocation);
+				GeckoPreferences.User["gfx.font_rendering.graphite.enabled"] = true;
 #else
 				// For windows, only initialize xulrunner if we are using the gecko browser control option
 				if (WeSayWordsProject.GeckoOption)
@@ -130,7 +124,6 @@ namespace WeSay.App
 					}
 					Xpcom.Initialize(xulRunnerLocation);
 					GeckoPreferences.User["gfx.font_rendering.graphite.enabled"] = true;
-					Console.WriteLine("XulRunner Initialized");
 				}
 #endif
 			}
