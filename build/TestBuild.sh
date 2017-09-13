7a024f9756c0c42c3a623cb531e993d72aa11693
glasseyes <dglassey@gmail.com>
2017-09-13 16:56:11 +0700

    get PACKAGE_VERSION working

--- build/TestBuild.sh
+++ build/TestBuild.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")/.."
 root=$PWD
 . environ
 cd  build
-xbuild "/target:${2:-Clean;Compile}" /p:Configuration="${1:-Debug}" /p:RootDir=$root  /p:BUILD_NUMBER="${3:0.0.1.abcd}" build.mono.proj
+xbuild "/target:${2:-Clean;Compile}" /p:Configuration="${1:-Debug}" /p:RootDir=$root  /p:BUILD_NUMBER="${3:-0.0.1.abcd}" build.mono.proj
